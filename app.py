1c7a9ef6704d1836dac920fc032109f3819f8bdf
lodex23 <jeffrey.lodewyck23@gmail.com>
2024-07-17 02:49:43 +0200

    Add styling to website and integrate logging
    Added a new CSS file for better site aesthetics, with styles for navigation, main content, and specific divisions. Modified the Python file to establish a logging system, enabling more precise tracking and recording of IP address and geographical coordinates. Also updated the website's HTML structure based on the new design.

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from flask import Flask, request, jsonify, render_template
 import geocoder
 import pdb
+import logging
 
 app = Flask(__name__)
+
+
+# Create a Logger
+logger = logging.getLogger('Location_writer')
+
+# Set the log level to Info
+logger.setLevel(logging.INFO)
+
+#Create a file handler for outputting to a file
+fh = logging.FileHandler('Location.log')
+
+fh.setLevel(logging.INFO)
+
+#Create a formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Add the logger to the Flask app
+app.logger.addHandler(fh)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 
 @app.route('/')
 def index():
         ip_address = request.remote_addr
         print(ip_address)
 
+        logger.info(ip_address)
 
         return render_template('index.html')
 
@@ -20,7 +42,8 @@ def get_location():
     # For simplicity, let's assume the location is provided in the request
     latitude = data.get('latitude')
     longitude = data.get('longitude')
-    return jsonify({'latitude': latitude, 'longitude': longitude})
+    logger.info(f"Latitude: {latitude}, Longitude: {longitude}")
+    return None
 
 if __name__ == '__main__':
         app.run(debug=True)
